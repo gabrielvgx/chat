11ee5cdcfe9c774b44d3c529e29072fd6aa16ddb
Gabrielvgx <gabrielvgx@gmail.com>
2017-10-06 13:26:20 -0300

    Comentario
    Apagar comentario Main

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -18,9 +18,6 @@ public class Chat extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
